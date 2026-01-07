8fa05b254a7fa002c1a999290bdaba71e333910d
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-01-08 02:06:29 +0300

    feat(protocol): add the consumer group membership api
    Implement JoinGroup, SyncGroup, Heartbeat and LeaveGroup request/response pairs, plus the Subscription and
    MemberAssignment DTOs used to encode/decode consumer group protocol metadata and partition assignments.

---
+++ src/Kafka/Protocol/Request/JoinGroupResponse.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * This file is part of the lisachenko/kafka-client package.
+ *
+ * (c) Alexander Lisachenko <lisachenko.it@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+/**
+ * @author Alexander.Lisachenko
+ * @date 14.07.2014
+ */
+
+namespace Protocol\Kafka\Protocol\Request;
+
+use Protocol\Kafka\IO\Stream;
+use Protocol\Kafka\Protocol\AbstractProtocolMessage;
+
+/**
+ * Join group response
+ */
+class JoinGroupResponse extends AbstractResponse
+{
+    /**
+     * Error code.
+     *
+     * @var integer
+     */
+    public $errorCode;
+
+    /**
+     * The generation of the consumer group.
+     *
+     * @var integer
+     */
+    public $generationId;
+
+    /**
+     * The group protocol selected by the coordinator
+     *
+     * @var string
+     */
+    public $groupProtocol;
+
+    /**
+     * The leader of the group
+     *
+     * @var string
+     */
+    public $leaderId;
+
+    /**
+     * The consumer id assigned by the group coordinator.
+     *
+     * @var string
+     */
+    public $memberId;
+
+    /**
+     * List of members of the group with metadata as value
+     *
+     * @var array
+     */
+    public $members = [];
+
+    /**
+     * Method to unpack the payload for the record
+     *
+     * @param AbstractProtocolMessage|static $self   Instance of current frame
+     * @param Stream $stream Binary data
+     *
+     * JoinGroup Response (Version: 0) => error_code generation_id group_protocol leader_id member_id [members]
+     *   error_code => INT16
+     *   generation_id => INT32
+     *   group_protocol => STRING
+     *   leader_id => STRING
+     *   member_id => STRING
+     *   members => member_id member_metadata
+     *     member_id => STRING
+     *     member_metadata => BYTES
+     *
+     * @return AbstractProtocolMessage
+     */
+    protected static function unpackPayload(AbstractProtocolMessage $self, Stream $stream): AbstractProtocolMessage
+    {
+        [$self->correlationId, $self->errorCode, $self->generationId] = array_values($stream->read('NcorrelationId/nerrorCode/NgenerationId'));
+
+        $self->groupProtocol = $stream->readString();
+        $self->leaderId      = $stream->readString();
+        $self->memberId      = $stream->readString();
+
+        $membersCount = $stream->read('NmembersCount')['membersCount'];
+        for ($memberIndex = 0; $memberIndex < $membersCount; $memberIndex++) {
+            $memberId   = $stream->readString();
+            $memberData = $stream->readByteArray();
+            $self->members[$memberId] = $memberData;
+        }
+
+        return $self;
+    }
+}
