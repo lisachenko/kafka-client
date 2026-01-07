bcd67bfc33243991f3ff3a15e3d0e09a9d099370
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-01-07 21:31:35 +0300

    feat(protocol): add throttle time to ProduceResponse v1
    Read the throttleTime field appended after the topic list in ProduceResponse, reporting how long the broker
    throttled the request due to a quota violation.

--- src/Kafka/Protocol/Request/ProduceResponse.php
+++ src/Kafka/Protocol/Request/ProduceResponse.php
@@ -34,6 +34,14 @@ class ProduceResponse extends AbstractResponse
     public $topics;
 
     /**
+     * Duration in milliseconds for which the request was throttled due to quota violation. (Zero if the request did not violate any quota).
+     *
+     * @var integer
+     * @since Version 1 of protocol
+     */
+    public $throttleTime;
+
+    /**
      * Method to unpack the payload for the record
      *
      * @param AbstractProtocolMessage|static $self   Instance of current frame
@@ -58,6 +66,8 @@ class ProduceResponse extends AbstractResponse
             }
 
         }
+        $self->throttleTime = $stream->read('NthrottleTime')['throttleTime'];
+
         return $self;
     }
 }
