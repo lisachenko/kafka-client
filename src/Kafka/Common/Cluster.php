3fd19eef830ee38510e2a387ebdd7eb1c8c4342d
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-01-10 09:22:39 +0300

    refactor(common): give Cluster a reload() API for refreshing metadata
    Cluster now stores its own configuration and exposes reload()/loadFromCache() so metadata can be
    re-fetched from a broker or restored from the cache after construction, instead of only being
    buildable once through the static bootstrap() factory.

--- src/Kafka/Common/Cluster.php
+++ src/Kafka/Common/Cluster.php
@@ -24,6 +24,8 @@ use Protocol\Kafka\IO\SocketStream;
 use Protocol\Kafka\Protocol\AbstractProtocolMessage;
 use Protocol\Kafka\Protocol\Request\MetadataRequest;
 use Protocol\Kafka\Protocol\Request\MetadataResponse;
+use Protocol\Kafka\Protocol\ApiKeys;
+use Protocol\Kafka\Common\Errors\UnknownErrorException;
 
 /**
  * A representation of a subset of the nodes, topics, and partitions in the ApiKeys cluster.
@@ -31,21 +33,29 @@ use Protocol\Kafka\Protocol\Request\MetadataResponse;
 final class Cluster
 {
     /**
+     * List of broker nodes
+     *
+     * @var Node[]|array
+     */
+    private $nodes = [];
+
+    /**
+     * Topic partitions
+     *
+     * @var TopicMetadata[]|array
+     */
+    private $topicPartitions = [];
+
+    /**
      * Creates a new cluster with the given nodes and partitions
+     *
+     * @param array $configuration Client configuration
      */
     private function __construct(
         /**
-         * List of broker nodes
-         *
-         * @var Node[]|array
+         * Client configuration
          */
-        private array $nodes,
-        /**
-         * Topic partitions
-         *
-         * @var TopicMetadata[]|array
-         */
-        private array $topicPartitions
+        private array $configuration
     ) {}
 
     /**
@@ -71,26 +81,20 @@ final class Cluster
      *
      * @return Cluster
      */
-    public static function bootstrap(array $configuration): self
+    public static function bootstrap(array $configuration): Cluster
     {
-        $brokerAddresses = [];
-        if (isset($configuration[ClientConfig::BOOTSTRAP_SERVERS])) {
-            $brokerAddresses = $configuration[ClientConfig::BOOTSTRAP_SERVERS];
-        };
+        $cluster        = new Cluster($configuration);
+        $isCacheEnabled = !empty($configuration[ClientConfig::METADATA_CACHE_FILE]);
+        $isLoaded       = false;
 
-        foreach ($brokerAddresses as $address) {
-            try {
-                $metadata = self::loadOrFetchMetadata($address, $configuration);
-                $cluster  = new Cluster($metadata->brokers, $metadata->topics);
-
-                return $cluster;
-            } catch (NetworkException) {
-                // we ignore all network errors and just try the next one address
-                continue;
-            }
+        if ($isCacheEnabled) {
+            $isLoaded = $cluster->loadFromCache();
         }
-        // If we here, we can't access any broker node, terminate
-        throw new NetworkException(['brokerAddresses' => $brokerAddresses]);
+        if (!$isLoaded) {
+            $cluster->reload();
+        }
+
+        return $cluster;
     }
 
     /**
@@ -174,6 +178,50 @@ final class Cluster
     }
 
     /**
+     * Reloads the metadata from the broker and optionally save it in the cache
+     *
+     * @throws ApiKeys\Error\UnknownError If information can not be reloaded
+     */
+    public function reload(): void
+    {
+        $brokerAddresses = [];
+        if (isset($this->configuration[ClientConfig::BOOTSTRAP_SERVERS])) {
+            $brokerAddresses = $this->configuration[ClientConfig::BOOTSTRAP_SERVERS];
+        };
+
+        foreach ($brokerAddresses as $address) {
+            try {
+                $stream  = new SocketStream($address, $this->configuration);
+                $request = new MetadataRequest();
+                $request->writeTo($stream);
+
+                $metadata = MetadataResponse::unpack($stream);
+                break;
+            } catch (NetworkException) {
+                // we ignore all network errors and just try the next one address
+                continue;
+            }
+        }
+        if (empty($metadata)) {
+            throw new UnknownErrorException(['error' => 'Can not fetch information about cluster metadata']);
+        }
+
+        $isCacheEnabled = !empty($this->configuration[ClientConfig::METADATA_CACHE_FILE]);
+        if ($isCacheEnabled) {
+            $milliSeconds = (int) (microtime(true) * 1e3);
+            $content      = '<?php return ' . var_export([$milliSeconds, $metadata], true) . ';';
+            $cacheFile    = $this->configuration[ClientConfig::METADATA_CACHE_FILE];
+            file_put_contents($cacheFile, $content);
+            if (function_exists('opcache_invalidate')) {
+                opcache_invalidate($cacheFile, true);
+            }
+        }
+
+        $this->nodes           = $metadata->brokers;
+        $this->topicPartitions = $metadata->topics;
+    }
+
+    /**
      * Get all topics
      *
      * @return string[]
@@ -184,48 +232,25 @@ final class Cluster
     }
 
     /**
-     * Queries metadata from the broker or loads this information from the cache file
+     * Loads cluster configuration from the cache
      *
-     * @param string $address Concrete broker address
-     * @param array $configuration
-     *
-     * @return AbstractProtocolMessage\MetadataResponse
+     * @return boolean True if metadata was successfully loaded from the cache
      */
-    private static function loadOrFetchMetadata($address, array $configuration)
+    private function loadFromCache(): bool
     {
-        $milliSeconds   = (int) (microtime(true) * 1e3);
-        $isCacheEnabled = !empty($configuration[ClientConfig::METADATA_CACHE_FILE]);
-        $metadata       = null;
+        $milliSeconds = (int) (microtime(true) * 1e3);
+        $cacheFile    = $this->configuration[ClientConfig::METADATA_CACHE_FILE];
+        if (is_readable($cacheFile)) {
+            /** @var AbstractProtocolMessage\MetadataResponse $metadata */
+            [$cachePutTimeMs, $metadata] = include $cacheFile;
+            if (($milliSeconds - $cachePutTimeMs) < $this->configuration[ClientConfig::METADATA_MAX_AGE_MS]) {
+                $this->nodes           = $metadata->brokers;
+                $this->topicPartitions = $metadata->topics;
 
-        if ($isCacheEnabled) {
-            $cacheFile = $configuration[ClientConfig::METADATA_CACHE_FILE];
-            if (is_readable($cacheFile)) {
-                [$cachePutTimeMs, $metadata] = include $cacheFile;
-            } else {
-                $cachePutTimeMs = $milliSeconds;
-            }
-            if (($milliSeconds - $cachePutTimeMs) > $configuration[ClientConfig::METADATA_MAX_AGE_MS]) {
-                $metadata = null;
+                return true;
             }
         }
 
-        if (empty($metadata)) {
-            $stream  = new SocketStream($address, $configuration);
-            $request = new MetadataRequest();
-            $request->writeTo($stream);
-
-            $metadata = MetadataResponse::unpack($stream);
-        }
-
-        if ($isCacheEnabled && isset($stream)) {
-            $content   = '<?php return ' . var_export([$milliSeconds, $metadata], true) . ';';
-            $cacheFile = $configuration[ClientConfig::METADATA_CACHE_FILE];
-            file_put_contents($cacheFile, $content);
-            if (function_exists('opcache_invalidate')) {
-                opcache_invalidate($cacheFile, true);
-            }
-        }
-
-        return $metadata;
+        return false;
     }
 }
