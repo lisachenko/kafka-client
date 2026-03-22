16e0ef9a0014396cc8fd32ea8218e24fd800187e
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-03-22 13:09:47 +0300

    feat(protocol): add MetadataResponse version 2 support for kafka 0.10.1
    Bump MetadataRequest to version 2 and read the new cluster_id field from MetadataResponse, matching the version 2 Metadata API introduced in kafka 0.10.1.

--- src/Kafka/Protocol/Request/MetadataResponse.php
+++ src/Kafka/Protocol/Request/MetadataResponse.php
@@ -38,6 +38,15 @@ class MetadataResponse extends AbstractResponse
     public $brokers = [];
 
     /**
+     * The cluster id that this broker belongs to.
+     *
+     * @since 0.10.1
+     *
+     * @var string
+     */
+    public $clusterId;
+
+    /**
      * The broker id of the controller broker.
      *
      * @var integer
@@ -69,6 +78,7 @@ class MetadataResponse extends AbstractResponse
 
             $self->brokers[$brokerNode->nodeId] = $brokerNode;
         }
+        $self->clusterId    = $stream->readString();
         $self->controllerId = $stream->read('NcontrollerId')['controllerId'];
         $numberOfTopics     = $stream->read('NnumberOfTopics')['numberOfTopics'];
 
